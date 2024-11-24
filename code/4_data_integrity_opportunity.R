bb0929a542ae2d7608b1f6481169835ff80c0f1b
Janarth Duraisingham <janarth.duraisingham@nhsbt.nhs.uk>
2024-11-24 20:49:00 +0000

    save plots in data integrity opportunity script

--- code/4_data_integrity_opportunity.R
+++ code/4_data_integrity_opportunity.R
@@ -1,4 +1,5 @@
-# Purpose: this script highlights GP practices that present opportunity to improve data collection and collation activities
+# Purpose: this script generates a histogram of the distribution of missing or bad data across GPs and identifies practices 
+# falling under extreme quantiles
 
 # Import libraries
 library(tidyverse)
@@ -6,7 +7,20 @@ library(tidyverse)
 # Import data 
 raw_data <- read_csv("data/task_dataset.csv")
 
-# Look at data lacking integrity or completeness
+# Create save directory
+if (!dir.exists("output")) {
+  
+  dir.create("output")
+  
+}
+
+if (!dir.exists("output/data_integrity_opportunity")) {
+  
+  dir.create("output/data_integrity_opportunity")
+  
+}
+
+# Sum columns indicating missing or bad data points
 bad_data <- raw_data %>%
   filter(icb_name %in% c("NHS North Central London Integrated Care Board",
                          "NHS North West London Integrated Care Board",
@@ -14,14 +28,14 @@ bad_data <- raw_data %>%
                          "NHS South East London Integrated Care Board",
                          "NHS South West London Integrated Care Board"
   )) %>%
-  filter(icb_name == "NHS North Central London Integrated Care Board") %>%
+  filter(icb_name == "NHS North Central London Integrated Care Board") %>% # COMMENT OUT TO LOOK AT ALL LONDON ICBS
   select(gp_code,
          AttendanceOutcome_Unknown,
          ApptModality_Unknown,
          BookingtoApptGap_UnknownDataIssue) %>%
   mutate(bad_datapoints = rowSums(across(c(AttendanceOutcome_Unknown, ApptModality_Unknown,BookingtoApptGap_UnknownDataIssue)), na.rm = TRUE))
 
-# PLot histogram of bad datapoints
+# PLot histogram of number of bad datapoints
 
 bad_data_hist <- ggplot(bad_data, aes(x=bad_datapoints)) +
   geom_histogram() +
@@ -30,7 +44,9 @@ bad_data_hist <- ggplot(bad_data, aes(x=bad_datapoints)) +
   xlab("Total Bad Datapoints") +
   ylab("Frequency")
 
-bad_data_hist
+ggsave("output/data_integrity_opportunity/bad_missing_data_histogram.png", bad_data_hist)
 
-filter(bad_data, bad_datapoints >= 1000)$gp_code
+opportunities <- filter(bad_data, bad_datapoints >= 1000)$gp_code
+
+write.csv(opportunities, "output/data_integrity_opportunity/gps_with_data_integrity_opportunity.csv")
          
