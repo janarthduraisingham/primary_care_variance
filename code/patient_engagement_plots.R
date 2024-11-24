eae829e65e5945468c6e345e9714fe8e44d6b92e
Janarth Duraisingham <janarth.duraisingham@nhsbt.nhs.uk>
2024-11-24 03:51:27 +0000

    add code to generate plots and quantiles for patient engagement

---
+++ code/patient_engagement_plots.R
@@ -0,0 +1,90 @@
+# Purpose: this script generates plots illustrating the engagement of patients with GP Practices
+
+# Import libraries
+library(tidyverse)
+
+# Import data
+raw_data <- read_csv("data/task_dataset.csv")
+
+# Set quantile variable for threshold
+quantile <- 0.9
+
+# Compute proportion of appointments not attended per practice
+metrics <- raw_data %>%
+  select(gp_code,
+         icb_name,
+         numberofpatients,
+         AttendanceOutcome_Attended,
+         AttendanceOutcome_DNA,
+         AttendanceOutcome_Unknown,
+         lastgpapptneeds,
+         overallexp,
+         gpcontactoverall) %>%
+  filter(icb_name %in% c("NHS North Central London Integrated Care Board",
+                         "NHS North West London Integrated Care Board",
+                         "NHS North East London Integrated Care Board",
+                         "NHS South East London Integrated Care Board",
+                         "NHS South West London Integrated Care Board"
+  )) %>%
+  mutate(total_appointments = AttendanceOutcome_Attended + AttendanceOutcome_DNA + AttendanceOutcome_Unknown) %>% # Compute total number of appointments
+  mutate(attended_prop = AttendanceOutcome_Attended / total_appointments, # express outcomes proportionally
+         dna_prop = AttendanceOutcome_DNA / total_appointments,
+         unknown_prop = AttendanceOutcome_Unknown / total_appointments)
+
+###
+### Unattended appointments
+###
+  
+# Plot histogram of proportion of unattended appointments
+unattended_hist <- ggplot(metrics, aes(x=dna_prop)) +
+  geom_histogram() +
+  theme_classic() +
+  ggtitle("Histogram of proportion of appointments not attended per practice") +
+  xlab("Proportion of appointments not attended") +
+  ylab("Frequency")
+
+unattended_hist
+
+high_dna <- metrics %>%
+  filter(dna_prop >= quantile(metrics$dna_prop, quantile, na.rm=TRUE))
+
+###
+### Appointment needs met
+###
+
+# Plot histogram of proportion of last appointments where needs where met
+needsmet_hist <- ggplot(metrics, aes(x=lastgpapptneeds)) +
+  geom_histogram() +
+  theme_classic() +
+  ggtitle("Histogram of proportion of 'Needs Met' at last appointment") +
+  xlab("Proportion of 'Needs Met' answered 'Yes' for last appointment") +
+  ylab("Frequency")
+
+needsmet_hist
+
+low_needsmet <- metrics %>%
+  filter(lastgpapptneeds <= quantile(metrics$lastgpapptneeds, 1-quantile, na.rm=TRUE))
+
+###
+### Overall Experience
+### 
+
+# Plot histogram of proportion of 'Good' answered to overall experience 
+overallexp_hist <- ggplot(metrics, aes(x=overallexp)) +
+  geom_histogram() +
+  theme_classic() +
+  ggtitle("Histogram of proportion of 'Overall Experience' answered Good") +
+  xlab("Proportion of 'Overall Experience' answered Good") +
+  ylab("Frequency")
+
+overallexp_hist
+  
+low_overallexp <- metrics %>%
+  filter(overallexp <= quantile(metrics$overallexp, 1-quantile, na.rm=TRUE))
+
+gps_to_support <- Reduce(intersect, list(high_dna$gp_code,
+                       low_needsmet$gp_code,
+                       low_overallexp$gp_code))
+
+gps_to_support
+length(gps_to_support)
